6c7657ebd4f6fa6765cc8a5f17d6bfe28a61b0c1
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2022-05-24 14:05:13 +0200

    fin de l'implantion html/css du site avec responsive

---
+++ pages/contact.php
@@ -0,0 +1,51 @@
+<?php require_once('../php/header_about.php') ?>
+<link rel="stylesheet"  href="../assets/css/contact.css">
+
+<div class='main'>
+    
+        <div class="contact_form">
+            <div class="contact">
+                <h1>Contact Info:</h1>
+                <div class="text">
+                    <p>To give give beginning divide, cattle. Give moving</p>
+                    <p>won't, there the abundantly she'd she'd brought air</p>
+                    <p>upon. Light hath subdue. Life days creature upon</p>
+                    <p>first heaven gathering dry.</p>
+                </div>
+                <div class="address_bloc">
+                    <p><span class="info">Address</span>: 10111 Santa Monica Boulevard, LA</p>
+                    <p><span class="info">Phone</span>: +44 987 065 908</p>
+                    <p><span class="info">Email</span>:info@Example.com</p>
+                    <p><span class="info">Phone</span>:+44 987 065 909</p>
+                </div>
+            </div>
+
+
+            <form>
+                <div class="name_mail">  
+                    <input type="text" name="name" id="name" placeholder="Your Name">
+                    <input type="text" name="email" id="email" placeholder="Your Email">
+                </div> 
+                <input type="text" name="title" id="title" placeholder="Your title">
+                <textarea id="textarea" placeholder="Your Comment"></textarea>
+                
+                <button type="submit" id="submit_btn">send message</button>
+               
+            </form>
+        </div>
+        <div class="map">
+
+        </div>
+
+    
+</div>
+
+
+
+
+
+
+
+
+<script src="../assets/js/index.js"></script>
+<?php require_once('../php/footer.php') ?>
